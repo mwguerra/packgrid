d8f6171331c223a0eb92ffe74742a79b7f32fa69
Marcelo Guerra <mwguerra@gmail.com>
2026-01-17 05:21:58 -0300

    feat: add Copy Composer Setup action to tokens list page
    Adds a header action that copies the Composer repositories configuration
    to clipboard, making it easier for users to configure their composer.json.

--- app/Filament/Resources/Tokens/Pages/ListTokens.php
+++ app/Filament/Resources/Tokens/Pages/ListTokens.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\Tokens\Pages;
 
 use App\Filament\Resources\Tokens\TokenResource;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListTokens extends ListRecords
@@ -17,7 +19,29 @@ class ListTokens extends ListRecords
 
     protected function getHeaderActions(): array
     {
+        $repositoriesJson = json_encode([
+            'repositories' => [
+                [
+                    'type' => 'composer',
+                    'url' => config('app.url'),
+                ],
+            ],
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
         return [
+            Action::make('copyComposerSetup')
+                ->label(__('token.action.copy_composer_setup'))
+                ->icon('heroicon-o-clipboard-document-list')
+                ->color('gray')
+                ->action(function (): void {
+                    Notification::make()
+                        ->title(__('token.notification.composer_setup_copied'))
+                        ->success()
+                        ->send();
+                })
+                ->extraAttributes([
+                    'x-on:click' => 'navigator.clipboard.writeText('.json_encode($repositoriesJson).')',
+                ]),
             CreateAction::make(),
         ];
     }
